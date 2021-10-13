48404a4a92e509d202b4577464913dbee65066f1
Kelsey <kdoyle390@gmail.com>
2021-10-12 19:35:07 -0500

    commit tree e53463fb42e7cbd617edf4cf8cc40203d886e2e2

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+let pickUpLocation;
+let destination;
+let feet;
+
+const distanceFromHqInBlocks = function(pickUpLocation) {
+    let blocks;
+    if (pickUpLocation > 42) {
+        blocks = pickUpLocation - 42; 
+        return blocks;
+    } else if (pickUpLocation < 42) {
+        blocks = 42 - pickUpLocation;
+        return blocks;
+    }   
+}
+
+ function distanceFromHqInFeet(pickUpLocation) {
+    feet = distanceFromHqInBlocks(pickUpLocation) * 264;
+    return feet;
+ }
+
+const distanceTravelledInFeet = function(pickUpLocation, destination) {
+    let distanceInBlocks;
+    if (pickUpLocation > destination) {
+        distanceInBlocks = pickUpLocation - destination; 
+    } else if (pickUpLocation < destination) {
+        distanceInBlocks = destination - pickUpLocation;
+    }
+    feet = distanceInBlocks * 264;
+    return feet;
+}
+    
+function calculatesFarePrice(pickUpLocation, destination) {
+    let fare;
+    feet = distanceTravelledInFeet(pickUpLocation, destination);
+    if (feet <= 400) {
+        fare = 0;
+        return fare;
+    } else if (feet > 400 && feet <= 2000) {
+        fare = (feet - 400) * 0.02;
+        return fare;
+    } else if (feet > 2000 && feet < 2500) {
+        return 25;
+    } else if (feet >= 2500) {
+        return 'cannot travel that far';
+    }
+}
